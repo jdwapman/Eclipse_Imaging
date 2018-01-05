15dba2ef6955b107d92411e430c9a297eaa25d40
jdwapman <jwapman@jonathans-mbp.attlocal.net>
2018-01-05 13:15:53 -0800

    Can be used with any user directory

--- src/main.cpp
+++ src/main.cpp
@@ -70,8 +70,8 @@ int main(int argc, char** argv )
 
 
 	/*----- SET UP FOLDER -----*/
-	//path p("/home/jwapman/Eclipse_Workspace/Target_Detection/Input_Images"); //Can select smaller folder
-	path p("/home/jwapman/Eclipse_Workspace/Target_Detection/Input_Images/Selected_Images"); //Can select smaller folder
+	//path p("~/Eclipse_Workspace/Target_Detection/Input_Images"); //Can select smaller folder
+	path p("~/Eclipse_Workspace/Target_Detection/Input_Images/Selected_Images"); //Can select smaller folder
 	recursive_directory_iterator end_itr;
 
     /*----- PROCESS ALL IMAGES IN FOLDER -----*/
@@ -276,7 +276,7 @@ void saveImage(Mat& img, string path)
 
 Mat getImage()
 {
-	return imread("/home/jwapman/Eclipse_Workspace/Target_Detection/Images/tarps.jpg", CV_LOAD_IMAGE_COLOR);
+	return imread("~/Eclipse_Workspace/Target_Detection/Images/tarps.jpg", CV_LOAD_IMAGE_COLOR);
 }
 
 
