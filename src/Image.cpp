c3fac0d979f9730bc01f86b1cb9070de7533f4b8
Jonathan Wapman <jdwapman@ucdavis.edu>
2018-02-09 15:22:52 -0800

    Initial Commit

---
+++ src/Image.cpp
@@ -0,0 +1,10 @@
+/*
+ * Image.cpp
+ *
+ *  Created on: Feb 9, 2018
+ *      Author: jwapman
+ */
+
+
+
+
---
+++ src/Image.h
@@ -0,0 +1,15 @@
+/*
+ * Image.h
+ *
+ *  Created on: Feb 9, 2018
+ *      Author: jwapman
+ */
+
+#ifndef SRC_IMAGE_H_
+#define SRC_IMAGE_H_
+
+
+
+
+
+#endif /* SRC_IMAGE_H_ */
