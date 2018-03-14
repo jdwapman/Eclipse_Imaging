d370ad77f1b43a543b75ea0a7a57c9f97d1a66fc
Jonathan Wapman <jdwapman@ucdavis.edu>
2018-03-14 15:08:19 -0700

    Modified launch script to require 1 input parameter

--- eclipse_launch.sh
+++ eclipse_launch.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
+if [ "$#" -ne 1 ]; then
+    echo "Incorrect number of parameters"
+	echo "Usage: ./eclipse_launch <numImages>"
+	echo "Input -1 for continuous capture"
+	exit -1
+fi
+
 cd /media/nvidia/SD_TX1/Output_Images/Camera_Images/
 
 date=$(date +'%Y-%m-%d_%H-%M-%S')
